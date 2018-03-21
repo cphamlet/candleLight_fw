04a7b8dd0471ed319bbdf4730c74e6317e9ba9bc
Connor Hamlet <cphamlet@live.unc.edu>
2018-03-21 11:45:00 -0400

    added 2c1 filter

--- vul_detection/filtered_out.py
+++ vul_detection/filtered_out.py
@@ -1,13 +1,15 @@
 import can
 
 can_interface = 'can0'
-bus = can.interface.Bus(can_interface, bustype='socketcan_native')
+bus = can.interface.Bus(can_interface, bustype='socketcan_native', canfilters={"can_id": 0x2c1, "can_mask": 0xff, "extended": False})
 
 #The message pool 
 message_pool = set()
 for message in bus:
     tmp_list = list(message.data)
     data_str = ' '.join(format(x,'x').capitalize() for x in tmp_list)
+    if message.arbitration_id == 0x2c1:
+        continue
     data_str = format(message.arbitration_id, 'x').capitalize() + "#" + data_str 
     if data_str not in message_pool:
         message_pool.add(data_str)
